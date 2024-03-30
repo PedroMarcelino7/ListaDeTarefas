4624891f8daee7a760ad57f3dc6328ee63019e6d
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-30 15:43:22 -0300

    creating getters and setters

--- classes/tarefa.model.php
+++ classes/tarefa.model.php
@@ -6,4 +6,12 @@ class Tarefa
     private $id_status;
     private $tarefa;
     private $data_cadastro;
+
+    public function __get($atributo){
+        return $this->$atributo;
+    }
+    
+    public function __Set($atributo, $valor){
+        $this->$atributo = $valor;
+    }
 }
