07f8a5809729579cea54c0fa1fe1044b85353ab5
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-30 18:30:31 -0300

    success task creation label

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -4,10 +4,6 @@ require "classes/tarefa.model.php";
 require "classes/tarefa.service.php";
 require "classes/conexao.php";
 
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
-
 $tarefa = new Tarefa();
 $tarefa->__set('tarefa', $_POST['tarefa']);
 
@@ -16,6 +12,4 @@ $conexao = new Conexao();
 $tarefaService = new TarefaService($conexao, $tarefa);
 $tarefaService->inserir();
 
-echo '<pre>';
-print_r($tarefaService);
-echo '</pre>';
+header('Location: nova_tarefa.php?inclusao=1');
