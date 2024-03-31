d5ed0dd4742fef0b09d5c51c6e3f43b147607084
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-30 22:17:35 -0300

    listing all tasks pt.1

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -4,12 +4,18 @@ require "classes/tarefa.model.php";
 require "classes/tarefa.service.php";
 require "classes/conexao.php";
 
-$tarefa = new Tarefa();
-$tarefa->__set('tarefa', $_POST['tarefa']);
+$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-$conexao = new Conexao();
+if ($acao == 'inserir') {
+    $tarefa = new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
 
-$tarefaService = new TarefaService($conexao, $tarefa);
-$tarefaService->inserir();
+    $conexao = new Conexao();
 
-header('Location: nova_tarefa.php?inclusao=1');
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->inserir();
+
+    header('Location: nova_tarefa.php?inclusao=1');
+} else if ($acao == 'recuperar') {
+    echo 'chegamos até aqui';
+}
