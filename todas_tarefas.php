dca7fba898ea11da9d57a1b9026b79e749fc568f
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-31 17:32:40 -0300

    completing tasks

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -56,8 +56,12 @@ require 'tarefa_controller.php';
 			tarefa.insertBefore(form, tarefa[0])
 		}
 
-		function remover(id){
+		function remover(id) {
 			location.href = `todas_tarefas.php?acao=remover&id=${id}`
+		}
+
+		function marcarRealizada(id) {
+			location.href = `todas_tarefas.php?acao=marcarRealizada&id=${id}`
 		}
 	</script>
 </head>
@@ -100,9 +104,13 @@ require 'tarefa_controller.php';
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"
 											onclick="remover(<?= $tarefa->id ?>)"></i>
-										<i class="fas fa-edit fa-lg text-info"
-											onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+
+										<?php if ($tarefa->status == 'pendente') { ?>
+											<i class="fas fa-edit fa-lg text-info"
+												onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+											<i class="fas fa-check-square fa-lg text-success"
+												onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+										<?php } ?>
 									</div>
 								</div>
 
