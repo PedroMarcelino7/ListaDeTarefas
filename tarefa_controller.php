6dc519cb8f3df0d78620492ebb7116d9fe07f48d
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-31 17:10:32 -0300

    updating tasks pt.2

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -18,7 +18,20 @@ if ($acao == 'inserir') {
     header('Location: nova_tarefa.php?inclusao=1');
 } else if ($acao == 'recuperar') {
     $tarefa = new Tarefa();
+
     $conexao = new Conexao();
+
     $tarefaService = new TarefaService($conexao, $tarefa);
     $tarefas = $tarefaService->recuperar();
+} else if ($acao == 'atualizar') {
+    $tarefa = new Tarefa();
+    $tarefa->__set('id', $_POST['id']);
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    if ($tarefaService->atualizar()) {
+        header('location: todas_tarefas.php');
+    }
 }
